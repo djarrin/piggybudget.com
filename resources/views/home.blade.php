34d1e7c693e6835e6fef3fe6176b4cd5e09ef3f6
David Jarrin <dmjarrin@gmail.com>
2017-01-11 19:18:55 -0500

    have set up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,17 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
+
+                @if( isset($userLoggedIn) )
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
                     You are logged in!
                 </div>
+                @else
+                <p>would you like to <a href="{{ url('/register') }}">Signup</a> or <a href="{{ url('/login') }}">log in</a>?</p>
+                @endif
+
             </div>
         </div>
     </div>
